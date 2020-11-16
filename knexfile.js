90555c5c4d7f47fb5b23214dd46b2d88bc2314db
justinmrks <justinmarks19@gmail.com>
2020-11-16 12:56:17 -0600

    postgresql

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,5 @@
+const pgConnection = process.env.DATABASE_URL;
+
 module.exports = {
   development: {
     client: "sqlite3",
@@ -17,6 +19,20 @@ module.exports = {
         // runs after a connection is made to the sqlite engine
         conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
       },
+    },
+  },
+  production: {
+    client: "postgresql",
+    connection: pgConnection,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: "./database/migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
     },
   },
 };
